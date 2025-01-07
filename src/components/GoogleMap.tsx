dedc61e4f7b5ae9edb4f894b6736d3af5942d2d7
kamil467 <kamil467@gmail.com>
2025-01-07 10:22:07 +0000

    map issue resolved

--- src/components/GoogleMap.tsx
+++ src/components/GoogleMap.tsx
@@ -4,7 +4,7 @@ const GoogleMap = () => {
   return (
     <div className="w-full h-[400px] relative">
       <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d902.2678517246349!2d55.33369385!3d25.2576583!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e5f5c6d6e2f4f4d%3A0xb5f9a8c4db6d550e!2sFONTE%20GENERAL%20TRADING%20LLC!5e0!3m2!1sen!2sae!4v1704622430283!5m2!1sen!2sae"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1804.3787633310155!2d55.30703873355741!3d25.24509059016152!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e5f439940578155%3A0x9755b6b615831e7f!2sFONTE%20GENERAL%20TRADING%20LLC!5e0!3m2!1sen!2snl!4v1736245183166!5m2!1sen!2snl"
         width="100%"
         height="100%"
         style={{ border: 0 }}
